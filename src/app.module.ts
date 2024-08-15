3d7070c481336b650905abd35a9733fa7b4625f1
Erickson Reyes <ercbluemonday@yahoo.com>
2024-08-15 21:49:07 +0800

    Introduce rate throttling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import {LeadsModule} from "./modules/leads/leads.module";
 import {PrismaService} from "./prisma.service";
+import {ThrottlerGuard, ThrottlerModule} from "@nestjs/throttler";
+import {APP_GUARD} from "@nestjs/core";
 
 @Module({
-  imports: [LeadsModule],
-  providers: [PrismaService]
+    imports: [
+        ThrottlerModule.forRoot([{
+            ttl: 60000,
+            limit: 30,
+        }]),
+        LeadsModule
+    ],
+    providers: [
+        PrismaService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard
+        }
+    ]
 })
-export class AppModule  implements NestModule{
-  configure(_: MiddlewareConsumer): any {
-  }
+export class AppModule implements NestModule {
+    configure(_: MiddlewareConsumer): any {
+    }
 }
